5417f5479e9f055a8531fbabaa00e3fddf1b3abb
LauraPerim <laurabatmanp@gmail.com>
2020-06-10 09:21:24 -0300

    alt

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded',function(){
 	let db = coDesConnect('https://app-codes-a3e27.firebaseio.com/')
 
 	db.download('/',function(data){
-		let context = data
+		let context = data["Aparelho"]["celular"]["iphone"]["duvidas"]
 
 		/* Referente à lista de categorias*/
 		coDesReplace('.categorias-pai',context)
